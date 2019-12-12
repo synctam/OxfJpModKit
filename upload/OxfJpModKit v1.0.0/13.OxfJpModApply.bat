bef44c6093c8b549cd07349b9487172b3559b975
synctam <synctam+bitbucket@gmail.com>
2019-12-12 20:43:10 +0900

    間違って登録したファイルを削除。

--- upload/OxfJpModKit v1.0.0/13.OxfJpModApply.bat
+++
@@ -1,8 +0,0 @@
-@rem Oxenfree ‚Ì“ú–{Œê‰»MOD‚Ìì¬B
-@SET PATH="OxfJpModKit\tools";%PATH%
-
-xcopy /y OxfJpModKit\JpMod\*.* Unity_Assets_Files\resources\Mono\Assembly-CSharp\
-
-
-@pause
-@exit /b
--- upload/OxfJpModKit v1.0.0/OxfJpModKit/_OxfJpModMaker_help.bat
+++
@@ -1,6 +0,0 @@
-@rem Oxenfree の日本語化MOD作成のヘルプを表示する。
-
-OxfJpModMaker.exe -h
-
-@pause
-@exit /b
